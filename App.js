aafdc3dd16625a721d3eda57150e53071b27ca2c
Raghav Chhabra <raghavchhabra0899@gmail.com>
2023-04-07 12:34:41 +0530

    first commit via cdn

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading=React.createElement("h1",
+{id:"container"},
+"Namaste React what about you");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
